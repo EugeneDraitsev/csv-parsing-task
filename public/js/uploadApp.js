94683881eea63c735f570758139e34ddec6a755d
Eugene Draitsev <ddrrai@gmail.com>
2015-03-10 21:10:07 +0300

    added graph on result dialog, fixed ui bugs

--- public/js/uploadApp.js
+++ public/js/uploadApp.js
@@ -18,28 +18,79 @@ angular.module('uploadApp', ['angularFileUpload', 'ui.bootstrap', 'ngRoute'])
             })
     })
     .controller('UploadController', ['$scope', 'FileUploader', '$modal', function ($scope, FileUploader, $modal) {
+        var processGraph;
         var uploader = $scope.uploader = new FileUploader({
             url: '/uploads/'
         });
 
-        uploader.onErrorItem = function (fileItem, response, status, headers) {
-            //console.info(response, status, headers);
-        };
-
         $scope.uploader.onSuccessItem = function (fileItem, response, status, headers, uploader) {
             fileItem.result = response;
-            console.log($scope.uploader);
-            console.log(uploader);
         };
 
         $scope.setModalData = function (item) {
-            $scope.modalName = item.file.name;
-            $scope.modalData = item.result;
+            $scope.modalData = item;
         }
 
-        $scope.closeAlert = function () {
-            $('#alert').remove();
-        }
+        $('#myModal').on('shown.bs.modal', function (e) {
+            processGraph.redraw($scope.modalData);
+        })
+
+         nv.addGraph(function (result) {
+            processGraph = nv.models.lineChart()
+                .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
+                .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
+                .transitionDuration(350)  //how fast do you want the lines to transition?
+                .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
+                .showYAxis(true)        //Show the y-axis
+                .showXAxis(true)        //Show the x-axis
+            ;
+
+            processGraph.xAxis     //Chart x-axis settings
+                .axisLabel('Time (ms)')
+                .tickFormat(function (d) {
+                    return d3.time.format('%X')(new Date(d));
+                });
+
+            processGraph.yAxis     //Chart y-axis settings
+                .axisLabel('Heap Memory Usage (MB)')
+                .tickFormat(d3.format('.02f'));
+
+            nv.utils.windowResize(function () {
+                processGraph.update()
+            });
+
+
+            processGraph.redraw = function (result) {
+                d3.select('#chart svg')   //Select the <svg> element you want to render the chart in.
+                    .datum(
+                    [
+                        {
+                            values: result.monitorStat.map(function (stat) {
+                                return {
+                                    x: stat.date,
+                                    y: stat.heap / (1024 * 1024)
+                                }
+                            }),      //values - represents the array of {x,y} data points
+                            key: 'Heap Usage (MB)', //key  - the name of the series.
+                            color: '#ff7f0e'  //color - optional: choose your own line color.
+                        },
+                        {
+                            values: result.monitorStat.map(function (stat) {
+                                return {
+                                    x: stat.date,
+                                    y: stat.totalHeap / (1024 * 1024)
+                                }
+                            }),      //values - represents the array of {x,y} data points
+                            key: 'Total Heap (MB)', //key  - the name of the series.
+                            color: '#575757'  //color - optional: choose your own line color.
+                        }
+                    ])         //Populate the <svg> element with chart data...
+                    .call(processGraph);          //Finally, render the chart!
+
+            }
+
+             return processGraph;
+        });
     }])
     .controller('ServerStateController', ['$scope', function ($scope) {
         var processGraph,
